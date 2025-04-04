eb4c88f5c7b1ffc1e1ae1960d1e5deeee1b5ce95
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2025-04-04 11:39:28 +0000

    update extractor pipeline

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 if __name__ == "__main__":
-    extractor = CryptoExtractor(ticker="BTC-USD")
-    ohlcv = extractor.extract_ohlcv()
-    news = extractor.extract_news(query="Bitcoin", from_date=(datetime.today() - relativedelta(months=1)).strftime("%Y-%m-%d"), to_date=datetime.today().strftime("%Y-%m-%d"))
-    extractor.save_raw_data(ohlcv, news)
+    ticker_query = {"BTC-USD": "Bitcoin", "ETH-USD": "Ethereum", "BNB-USD": "Binance"}
+    for tick, quer in ticker_query.items():
+        extractor = CryptoExtractor(ticker=tick)
+        ohlcv = extractor.extract_ohlcv()
+        news = extractor.extract_news(query=quer, from_date=(datetime.today() - relativedelta(months=1)).strftime("%Y-%m-%d"), to_date=datetime.today().strftime("%Y-%m-%d"))
+        
+        extractor.save_raw_data(ohlcv, news)
